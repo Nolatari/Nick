8467aca167f890c7cdb7ce81ab0658aec3750af0
Randal Vanheede <randal.vanheede@entityone.be>
2020-03-18 12:51:21 +0100

    Additional changes.

--- web/index.php
+++ web/index.php
@@ -15,10 +15,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../settings.php';
 require_once __DIR__ . '/core/includes.php';
 
-$include_file = $_GET['p'] ?? 'dashboard';
-$include_file = 'pages/' . $include_file . '.php';
-if (is_file($include_file)) {
-  include $include_file;
+$page = $_GET['p'] ?? 'dashboard';
+$page = 'pages/' . $page . '.php';
+if (is_file($page)) {
+  include $page;
 }
 
 d(\Nick::Config()->difference());
