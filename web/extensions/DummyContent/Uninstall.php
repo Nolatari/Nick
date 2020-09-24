9b51a677fcb403d50b7c87eb93661af1fa1c92da
Randal Vanheede <randal.vanheede@entityone.be>
2020-09-24 12:34:34 +0200

    Added uninstall script for dummycontent extension

---
+++ web/extensions/DummyContent/Uninstall.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Nick\DummyContent;
+
+use Nick;
+use Nick\ExtensionManager\InstallInterface;
+use Nick\ExtensionManager\UninstallInterface;
+use Nick\Page\PageManager;
+
+/**
+ * Class Uninstall
+ *
+ * @package Nick\DummyContent
+ */
+class Uninstall implements UninstallInterface {
+
+  /** @var PageManager $pageManager */
+  protected PageManager $pageManager;
+
+  /**
+   * install constructor.
+   */
+  public function __construct() {
+    $this->pageManager = new PageManager();
+  }
+
+  /**
+   * Checks whether the page already exists
+   *
+   * @return bool
+   */
+  public function condition(): bool {
+    $pageObject = $this->pageManager->getPageObject('dummycontent');
+    if (!$pageObject instanceof DummyContent) {
+      return FALSE;
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function doUninstall() {
+    $db = Nick::Database()
+      ->delete('pages')
+      ->condition('id', 'dummycontent');
+    return $db->execute();
+  }
+}
