ffc5b4f6429413cc0620a5154011bcb3639f3070
Randal Vanheede <randal.vanheede@entityone.be>
2020-12-04 12:05:23 +0100

    Refactoring of the Rest client

--- web/core/components/Rest/Rest.php
+++ web/core/components/Rest/Rest.php
@@ -5,6 +5,7 @@ namespace Nick\Rest;
 use Nick\Entity\EntityInterface;
 use Nick\Rest\Entity\Client;
 use Nick\Rest\Entity\ClientInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Class Rest
@@ -18,11 +19,9 @@ class Rest {
    *
    * @param     $message
    * @param int $status
-   *
-   * @return array|int[]
    */
   public static function message($message, int $status = 400) {
-    return static::data([], $message, $status);
+    static::data([], $message, $status);
   }
 
   /**
@@ -31,8 +30,6 @@ class Rest {
    * @param array       $data
    * @param int         $status
    * @param string|null $message
-   *
-   * @return array
    */
   public static function data(array $data = [], int $status = 200, ?string $message = NULL) {
     $return_array = ['status' => $status ?? 200];
@@ -42,96 +39,93 @@ class Rest {
     if (!empty($message)) {
       $return_array['message'] = $message;
     }
-    return $return_array;
+
+    $response = new JsonResponse();
+    $response->setContent(json_encode($return_array));
+    $response->setStatusCode($status);
+    $response->send();
+    exit;
+  }
+
+  /**
+   * @param array $information
+   *
+   * @return bool
+   */
+  public static function auth(array $information): bool {
+    if (!$information['uuid']) {
+      static::message('No authorization method was given.', 401);
+    }
+
+    /** @var Client $client */
+    $client = \Nick::EntityManager()
+      ->loadByProperties([
+        'type' => 'client',
+        'uuid' => $information['uuid'],
+      ]);
+    if (!$client instanceof ClientInterface) {
+      static::message('Client does not exist.', 401);
+    }
+    if (!$client->hasPermission('retrieve')) {
+      static::message('Client has no access to this function.', 405);
+    }
+
+    return TRUE;
   }
 
   /**
    * Retrieves information from Nick to the front-end
    *
-   * @param $information
-   *
-   * @return array
+   * @param array $information
    */
-  public static function Retrieve($information): array {
-    if (!$information['uuid']) {
-      return ['message' => 'No authorization method was given.'];
-    }
-
-    /** @var Client $client */
-    $client = \Nick::EntityManager()
-      ->loadByProperties([
-        'type' => 'client',
-        'uuid' => $information['uuid'],
-      ]);
-    if (!$client instanceof ClientInterface) {
-      return static::message('Client does not exist.', 401);
-    }
-    if (!$client->hasPermission('retrieve')) {
-      return static::message('Client has no access to this function.', 405);
-    }
+  public static function Retrieve(array $information) {
+    static::auth($information);
 
     if (!isset($information['entity'])) {
-      return static::message('No entity was given.', 400);
+      static::message('No entity was given.', 400);
     }
 
     /** @var EntityInterface $data */
     $data = \Nick::EntityManager()
       ->loadByProperties($information['entity']);
     if (!$data instanceof EntityInterface) {
-      return static::message('Requested entity does not exist or is corrupt.', 404);
+      static::message('Requested entity does not exist or is corrupt.', 404);
     }
 
-    return static::data($data->getValues(), 200, 'Data was successfully retrieved.');
+    static::data($data->getValues(), 200, 'Data was successfully retrieved.');
   }
 
   /**
    * Transmits information from the front-end to the Nick
    *
-   * @param $information
-   *
-   * @return array
+   * @param array $information
    */
-  public static function Transmit($information): array {
-    if (!$information['uuid']) {
-      return ['message' => 'No authorization method was given.'];
-    }
-
-    /** @var Client $client */
-    $client = \Nick::EntityManager()
-      ->loadByProperties([
-        'type' => 'client',
-        'uuid' => $information['uuid'],
-      ]);
-    if (!$client instanceof ClientInterface) {
-      return ['message' => 'Client does not exist.'];
-    }
-    if (!$client->hasPermission('transmit')) {
-      return ['message' => 'Client has no access to this function.'];
-    }
+  public static function Transmit(array $information) {
+    static::auth($information);
 
     if (!isset($information['entity'])) {
-      return static::message('No entity was given.', 400);
+      static::message('No entity was given.', 400);
     }
 
     if (!isset($information['new-values'])) {
-      return static::message('No new values were given, can\'t save entity. [usage: add data key "new-values" with array of values]', 412);
+      static::message('No new values were given, can\'t save entity. [usage: add data key "new-values" with array of values]', 412);
     }
 
     /** @var EntityInterface $data */
     $data = \Nick::EntityManager()
       ->loadByProperties($information['entity']);
     if (!$data instanceof EntityInterface) {
-      return static::message('Requested entity does not exist or is corrupt.', 404);
+      static::message('Requested entity does not exist or is corrupt.', 404);
     }
 
     foreach ($information['new-values'] as $key => $value) {
       $data->setValue($key, $value);
     }
     if (!$data->save()) {
-      return static::message('Something went wrong trying to save the entity', 400);
+      static::message('Something went wrong trying to save the entity', 400);
     }
 
-    return static::data($data->getValues(), 200, 'Data was saved to the database');
+    static::data($data->getValues(), 200, 'Data was saved to the database');
   }
 
 
