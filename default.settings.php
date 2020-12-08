74e3f8ec7e9b6510e5b4470c6c8717d5f4212448
Randal Vanheede <randal.vanheede@gmail.com>
2020-12-08 20:52:29 +0100

    Bugfixes

--- default.settings.php
+++ default.settings.php
@@ -2,36 +2,40 @@
 
 $nick_settings = [];
 
-// Default subsite
-$nick_settings['default'] = [
-  'url' => 'localhost',
-  'database' => [
-    'type' => 'mysql',
-    'hostname' => 'hostname',
-    'username' => 'username',
-    'database' => 'database',
-    'password' => 'password',
-    'port' => '3306',
-  ],
-  'root' => [
-    'folder' => __DIR__,
-    'web' => [
-      'url' => 'http://localhost',
-      'root' => '/',
-    ],
-  ],
-  'config' => [
-    'folder' => __DIR__ . '/config',
-  ],
-  'themes' => [
-    'folder' => 'themes',
-    'url' => $nick_settings['root']['web']['url'] . '/themes',
-  ],
-  'files' => [
-    'public' => $nick_settings['root']['web']['url'] . '/files/default/public',
-    'private' => $nick_settings['root']['web']['url'] . '/files/default/private',
-  ],
-  'debugging' => FALSE,
-  'twig_debugging' => FALSE,
-  'development' => FALSE,
+// ---------------------------------------- //
+//         Start Default subsite            //
+// ---------------------------------------- //
+$nick_settings['default'] = [];
+$nick_settings['default']['url'] = 'localhost';
+$nick_settings['default']['database'] = [
+  'type' => 'mysql',
+  'hostname' => 'hostname',
+  'username' => 'username',
+  'database' => 'database',
+  'password' => 'password',
+  'port' => '3306',
 ];
+$nick_settings['default']['root'] = [
+  'folder' => __DIR__,
+  'web' => [
+    'url' => 'http://localhost',
+    'root' => '/',
+  ],
+];
+$nick_settings['default']['config'] = [
+  'folder' => __DIR__ . '/config',
+];
+$nick_settings['default']['themes'] = [
+  'folder' => __DIR__ . '/web/themes',
+  'url' => $nick_settings['default']['root']['web']['url'] . '/themes',
+];
+$nick_settings['default']['files'] = [
+  'public' => $nick_settings['default']['root']['web']['url'] . '/files/default/public',
+  'private' => $nick_settings['default']['root']['web']['url'] . '/files/default/private',
+];
+$nick_settings['default']['debugging'] = FALSE;
+$nick_settings['default']['twig_debugging'] = FALSE;
+$nick_settings['default']['development'] = FALSE;
+// ---------------------------------------- //
+//           End Default subsite            //
+// ---------------------------------------- //
